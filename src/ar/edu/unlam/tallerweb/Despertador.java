6c40704c44850135379a4b2ce8b5ac8d6a366e10
AldanaDiFlavio <aldanadiflavio@live.com.ar>
2016-04-29 16:44:30 -0300

    Added files via upload

---
+++ src/ar/edu/unlam/tallerweb/Despertador.java
@@ -0,0 +1,26 @@
+package ar.edu.unlam.tallerweb;
+
+public class Despertador {
+	
+	//atributos
+	Integer horaAlarma;
+	Integer minutoAlarma;
+	
+	//metodos
+	public Despertador(){
+		
+	}
+
+	public void sonar(){
+		
+	}
+	
+	public void programar(Integer hora, Integer minuto){
+		horaAlarma=hora;
+		minutoAlarma=minuto;
+	}
+	
+	public void apagar(){
+		
+	}
+}
